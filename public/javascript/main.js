cc83625ac613fe88af8352ea3f682f113b814f82
jennieisa <jennieisakssoon97@gmail.com>
2022-05-18 11:56:29 +0200

    Added main.js index.html and style.css. function that render the accounts

---
+++ public/javascript/main.js
@@ -0,0 +1,46 @@
+const accountList = document.querySelector(".accountList");
+const createNewAccBtn = document.querySelector(".createNewAccBtn");
+const createNewAccForm = document.querySelector(".createNewAccForm");
+
+let accounts = [ ];
+
+//Template för att rita ut bankkonton
+const accountTemplate = (account) => `
+    <li>
+        <p>Namn: ${account.name}</p>
+        <p>Kontonummer: ${account._id}</p>
+        <p>Saldo: ${account.balance} kr</p>
+        <button data-function="addMoney" data-postid="${account._id}">Sätt in pengar</button>
+        <button data-function="takeOutMoney" data-postid="${account._id}">Ta ut pengar</button>
+        <button data-function="deleteAccount" data-postid=${account._id}>Avsluta konto</button>
+    </li>
+`;
+
+//Hämtar alla bankkonton och ritar ut 
+const drawAccounts = async () => {
+
+    const res = await fetch('/api/accounts');
+    accounts = await res.json();
+
+    accountList.innerHTML = accounts.map(accountTemplate).join('');
+
+}
+
+drawAccounts();
+
+//Visar formulär om vi klickar på skapa nytt konto 
+createNewAccBtn.addEventListener("click", () => {
+
+    createNewAccForm.style.display = "block";
+
+    accountList.style.display = "none";
+
+    createNewAccBtn.style.display = "none";
+
+})
+
+createNewAccForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+})
--- server.js
+++ server.js
@@ -21,6 +21,46 @@ await client.connect();
 const db = client.db('bank');
 const accountsCollection = db.collection('accounts');
 
+/*ROUTES FÖR KONTON */
+
+//Route för att hämta alla konton 
+app.get('/api/accounts', async (req, res) => {
+
+    let accounts = await accountsCollection.find({}).toArray();
+
+    res.json(accounts);
+
+})
+
+//Route för att lägga till ett nytt konto 
+app.post('/api/createnewaccount', async (req, res) => {
+
+    const account = {
+        ...req.body
+    };
+
+    await accountsCollection.insertOne(account);
+
+    res.json({
+        success: true,
+        account
+    });
+})
+
+//Route för att lägga till mer pengar på konto 
+
+//Route för att ta bort pengar från konto (tänk på att det inte ska gå att ta bort mer än det finns)
+
+//Route för att ta bort kontot helt
+
+/*ROUTES FÖR USERS*/
+
+//Route för att skapa ny användare
+
+//Route för att logga in
+
+//Route för att kontrollera om personen är inloggad 
+
 app.listen(port, () => {
 
     console.log(`listening on port ${port}`);
